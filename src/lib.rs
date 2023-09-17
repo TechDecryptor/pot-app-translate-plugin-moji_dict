ee1a74d15e23f26f12d743c0f8259026b2cce200
TechDecryptor <145176897+TechDecryptor@users.noreply.github.com>
2023-09-17 08:32:23 +0000

    1.0.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,65 @@
-use serde_json::Value;
+use serde_json::{json, Value};
 use std::collections::HashMap;
 use std::error::Error;
-use urlencoding::encode;
 
 #[no_mangle]
 pub fn translate(
-    text: &str, // 待翻译文本
-    from: &str, // 源语言
-    to: &str,   // 目标语言
-    // (pot会根据info.json 中的 language 字段传入插件需要的语言代码，无需再次转换)
-    needs: HashMap<String, String>, // 插件需要的其他参数,由info.json定义
+    text: &str,
+    _from: &str,
+    _to: &str,
+    _needs: HashMap<String, String>,
 ) -> Result<Value, Box<dyn Error>> {
     let client = reqwest::blocking::ClientBuilder::new().build()?;
-
-    let mut url = match needs.get("requestPath") {
-        Some(url) => url.to_string(),
-        None => return Err("requestPath not found".into()),
-    };
-
-    if !url.starts_with("http") {
-        url = format!("https://{}", url);
+    const URL: &str = "https://api.mojidict.com/parse/functions/union-api";
+    if text.split_whitespace().collect::<Vec<&str>>().len() > 1 {
+        return Ok(Value::String("".to_string()));
     }
-
-    let plain_text = text.replace("/", "@@");
-    let encode_text = encode(plain_text.as_str());
-
+    let body = json!({
+        "functions": [
+            {
+                "name": "search-all",
+                "params": {
+                    "text": text,
+                    "types": [102],
+                },
+            },
+        ],
+        "_ClientVersion": "js3.4.1",
+        "_ApplicationId": "E62VyFVLMiW7kvbtVq3p",
+        "g_os": "PCWeb",
+        "g_ver": "v4.6.4.20230615",
+        "_InstallationId": "1f7dbb56-9030-4f32-9645-0df69f86c591",
+    });
     let res: Value = client
-        .get(format!("{url}/api/v1/{from}/{to}/{encode_text}"))
+        .post(URL)
+        .header("Content-Type", "application/json;charset=UTF-8")
+        .json(&body)
         .send()?
         .json()?;
 
-    fn parse_result(res: Value) -> Option<String> {
-        let result = res.as_object()?.get("translation")?.as_str()?.to_string();
-
-        Some(result.replace("@@", "/"))
+    fn parse_result(res: Value) -> Option<Value> {
+        let mut explanations = Vec::new();
+        let list = res
+            .as_object()?
+            .get("result")?
+            .as_object()?
+            .get("results")?
+            .as_object()?
+            .get("search-all")?
+            .as_object()?
+            .get("result")?
+            .as_object()?
+            .get("word")?
+            .as_object()?
+            .get("searchResult")?
+            .as_array()?;
+        for i in list {
+            explanations.push(json!({"trait":"","explains":[i.as_object()?.get("title"),i.as_object()?.get("excerpt")]}))
+        }
+        Some(json!({"explanations":explanations}))
     }
     if let Some(result) = parse_result(res) {
-        return Ok(Value::String(result));
+        return Ok(result);
     } else {
         return Err("Response Parse Error".into());
     }
@@ -47,9 +70,8 @@ mod tests {
     use super::*;
     #[test]
     fn try_request() {
-        let mut needs = HashMap::new();
-        needs.insert("requestPath".to_string(), "lingva.pot-app.com".to_string());
-        let result = translate("你好 世界！", "auto", "en", needs).unwrap();
-        println!("{result}");
+        let needs = HashMap::new();
+        let result = translate("文具", "auto", "ja", needs).unwrap();
+        println!("{result:?}");
     }
 }
